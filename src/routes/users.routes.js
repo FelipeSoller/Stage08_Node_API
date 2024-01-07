282d4d9b4d447c43213cd6a0303cc7413cd2810b
Felipe Soller <soller@outlook.com>
2024-01-07 14:28:50 -0300

    handle exceptions errors

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,18 +4,19 @@ const UsersController = require('../controllers/UsersController')
 
 const userRoutes = Router()
 
-function myMiddleware(request, response, next) {
-  console.log('You have pass by our middleware');
+// function myMiddleware(request, response, next) {
+//   console.log('You have pass by our middleware');
 
-  if(!request.body.isAdmin) {
-    return response.json({ message: 'User unauthorized' })
-  }
+//   if(!request.body.isAdmin) {
+//     return response.json({ message: 'User unauthorized' })
+//   }
 
-  next()
-}
+//   next()
+// }
 
 const usersController =  new UsersController()
 
-userRoutes.post('/', myMiddleware, usersController.create)
+// userRoutes.post('/', myMiddleware, usersController.create)
+userRoutes.post('/', usersController.create)
 
 module.exports = userRoutes
