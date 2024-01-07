f1cf1c033841e1c9462506fd6453ca2b4395ce9e
Felipe Soller <soller@outlook.com>
2024-01-07 13:34:51 -0300

    learning route and query params

--- src/server.js
+++ src/server.js
@@ -2,6 +2,24 @@ const express = require('express')
 
 const app = express()
 
-const PORT = 3333
+app.get('/message/:id/:user', (request, response) => {
+  const { id, user } = request.params
+
+  response.send(`
+    ID da mensagem: ${id}.
+    Para o usuário: ${user}
+  `)
+})
+
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query
+
+  response.send(`
+    Page: ${page}.
+    Limit: ${limit}
+  `)
+})
+
+const PORT = 3000
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
