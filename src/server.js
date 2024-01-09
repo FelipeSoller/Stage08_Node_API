e3d0444e7d6ec9f6c4a0734d988db81e214fb848
Felipe Soller <soller@outlook.com>
2024-01-08 22:41:20 -0300

    config, call and create sqlite db

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 require('express-async-errors')
 
-const express = require('express')
-
+const database = require('./database/sqlite')
+const express  = require('express')
 const AppError = require('./utils/AppError')
-const routes = require('./routes')
+const routes   = require('./routes')
+const app      = express()
 
-const app = express()
+database()
+
 app.use(express.json())
-
 app.use(routes)
-
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
     return response.status(error.statusCode).json({
