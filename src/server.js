a791292d282624e11788e94add7aef3925b2915e
Felipe Soller <soller@outlook.com>
2024-02-02 13:28:20 -0300

    create new route to update avatar

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ require('express-async-errors')
 const migrationsRun = require('./database/sqlite/migrations')
 const express  = require('express')
 const AppError = require('./utils/AppError')
+const uploadConfig = require('./configs/upload')
 const routes   = require('./routes')
 const app      = express()
 
 migrationsRun()
 
 app.use(express.json())
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes)
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
