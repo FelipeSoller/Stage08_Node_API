d8cb5267df62bfb3f455a8a0bdb8b869eae1aee7
Felipe Soller <soller@outlook.com>
2024-02-27 15:33:18 -0300

    install and configure dotenv and pm2

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv/config')
 require('express-async-errors')
 
 const migrationsRun = require('./database/sqlite/migrations')
@@ -31,6 +32,6 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = 3000
+const PORT = process.env.SERVER_PORT || 3333
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
