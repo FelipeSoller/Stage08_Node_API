0a32ecd4b12e592a4d25cd202eaaf00f6f6e2212
Felipe Soller <soller@outlook.com>
2024-03-01 17:44:37 -0300

    refactoring to decouple data storage layer and business rules from the user controller

---
+++ src/services/UserCreateService.js
@@ -0,0 +1,26 @@
+const { hash, compare } = require('bcryptjs')
+
+const AppError = require('../utils/AppError')
+
+class UserCreateService {
+  constructor(usersRepository) {
+    this.usersRepository = usersRepository
+  }
+
+  async execute({ name, email, password }) {
+
+    const checkUserExists = await this.usersRepository.findByEmail(email)
+
+    if(checkUserExists) {
+      throw new AppError('This email is already in use')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    const userCreated = await this.usersRepository.create({ name, email, password: hashedPassword })
+
+    return userCreated
+  }
+}
+
+module.exports = UserCreateService
