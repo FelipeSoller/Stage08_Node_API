3e84cb7266bfd17bc285a1eae2e2eacaf267adff
Felipe Soller <soller@outlook.com>
2024-03-04 14:48:13 -0300

    refactor tests to use befor each

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -4,6 +4,14 @@ const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
 const AppError = require("../utils/AppError")
 
 describe('UserCreateService', () => {
+  let userRepositoryInMemory = null
+  let userCreateService = null
+
+  beforeEach(() => {
+    userRepositoryInMemory = new UserRepositoryInMemory()
+    userCreateService = new UserCreateService(userRepositoryInMemory)
+  })
+
   it('user should be created', async () => {
     const user = {
       name: 'John Doe',
@@ -11,8 +19,6 @@ describe('UserCreateService', () => {
       password: '123456'
     }
 
-    const userRepositoryInMemory = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepositoryInMemory)
     const userCreated = await userCreateService.execute(user)
 
     expect(userCreated).toHaveProperty('id')
@@ -30,9 +36,6 @@ describe('UserCreateService', () => {
       email: 'user@example.com',
       password: '123456'
     }
-
-    const userRepositoryInMemory = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepositoryInMemory)
 
     await userCreateService.execute(user1)
     await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError('This email is already in use'))
